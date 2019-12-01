6a7b8cae750dc6086d386bab48ff3b4a1f7f15bf
Michael Griffin <mdgriffin064@gmail.com>
2019-12-01 23:38:11 +0000

    Initial commit

---
+++ src/main/java/com/mdgriffin/adventOfCode2019/day1/FuelCounterUpper.java
@@ -0,0 +1,10 @@
+package com.mdgriffin.adventOfCode2019.day1;
+
+public class FuelCounterUpper {
+
+	public static int calculateRequiredFuel(int[] modules ) {
+
+
+		return 1;
+	}
+}
---
+++ src/test/java/com/mdgriffin/adventOfCode2019/day1/FuelCounterUpperTest.java
@@ -0,0 +1,13 @@
+package com.mdgriffin.adventOfCode2019.day1;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class FuelCounterUpperTest {
+
+	@Test
+	public void whenCalculatingRequiredFuel_correctResult() {
+		fail();
+	}
+}
