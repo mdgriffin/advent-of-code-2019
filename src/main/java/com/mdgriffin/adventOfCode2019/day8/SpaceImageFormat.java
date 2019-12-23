c4beec22ff569a3c04e921c96f920bf615199ba2
Michael Griffin <mdgriffin064@gmail.com>
2019-12-23 17:35:52 +0000

    Day 8 - Processing image string into int array

--- src/main/java/com/mdgriffin/adventOfCode2019/day8/SpaceImageFormat.java
+++ src/main/java/com/mdgriffin/adventOfCode2019/day8/SpaceImageFormat.java
@@ -1,5 +1,38 @@
 package com.mdgriffin.adventOfCode2019.day8;
 
+import java.util.Arrays;
+
 public class SpaceImageFormat {
+	
+	public int[][] layers;
+	private int width;
+	private int height;
+	
+	
+	public SpaceImageFormat (int width,int height, String encodedImage) {
+		this.width = width;
+		this.height = height;
+		
+		processImage(encodedImage);
+	}
+	
+	private void processImage(String encodedImage) {
+		int[] imageInts = Arrays.stream(encodedImage.replace("\n", "").split(""))
+			    .mapToInt(Integer::parseInt)
+			    .toArray();
+		int numBitsPerImage = width * height;
+		layers = new int[imageInts.length / numBitsPerImage][numBitsPerImage];
+		
+		for (int i = 0; i < layers.length; i++) {
+			int[] currentLayer = new int[numBitsPerImage];
+			
+			for (int j = 0; j < numBitsPerImage; j++) {
+				currentLayer[j] = imageInts[(i * numBitsPerImage) + j];
+				
+			}
+			
+			layers[i] = currentLayer;
+		}
+	}
 
 }
--- src/test/java/com/mdgriffin/adventOfCode2019/day8/SpaceImageFormatTest.java
+++ src/test/java/com/mdgriffin/adventOfCode2019/day8/SpaceImageFormatTest.java
@@ -1,5 +1,6 @@
 package com.mdgriffin.adventOfCode2019.day8;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,10 +10,21 @@ import com.mdgriffin.adventOfCode2019.common.FileReader;
 public class SpaceImageFormatTest {
 
 	private static final String PROVIDED_INPUT = FileReader.readFile("/test-input/day8.txt");
+	private static int IMAGE_WIDTH = 25;
+	private static int IMAGE_HEIGHT = 6;
+	
 	
 	@Test
 	public void dataLoaded() {
 		assertTrue(PROVIDED_INPUT.length() > 0);
+	}
+	
+	@Test
+	public void canCreate() {
+		SpaceImageFormat image = new SpaceImageFormat(IMAGE_WIDTH, IMAGE_HEIGHT, PROVIDED_INPUT);
+		
+		assertEquals(100, image.layers.length);
+		assertEquals(IMAGE_WIDTH * IMAGE_HEIGHT, image.layers[0].length);
 	}
 
 }
