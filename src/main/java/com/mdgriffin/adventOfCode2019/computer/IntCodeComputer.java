685f61401b8b76939f8446dce42e90cc15a11420
Michael Griffin <mdgriffin064@gmail.com>
2020-01-04 00:21:12 +0000

    Merged duplicated run and runToNextOutput on IntCodeComputer

--- src/main/java/com/mdgriffin/adventOfCode2019/computer/IntCodeComputer.java
+++ src/main/java/com/mdgriffin/adventOfCode2019/computer/IntCodeComputer.java
@@ -1,6 +1,7 @@
 package com.mdgriffin.adventOfCode2019.computer;
 
 import java.util.LinkedList;
+import java.util.function.Predicate;
 
 public class IntCodeComputer {
 
@@ -14,8 +15,8 @@ public class IntCodeComputer {
 		this.memory = memory.clone();
 	}
 
-	public LinkedList<Integer> run(LinkedList<Integer> inputs) {
-		while (isRunning && instructionPointer < memory.length) {
+	private LinkedList<Integer> run(LinkedList<Integer> inputs, Predicate<Integer> runCondition) {
+		while (runCondition.test(outputs.size()) && isRunning && instructionPointer < memory.length) {
 			int currentInstruction = memory[instructionPointer];
 			OpParams op = OpParams.getByInstruction(currentInstruction);
 			int args[] = new int[4];
@@ -68,65 +69,20 @@ public class IntCodeComputer {
 			instructionPointer += op.operation.getNumArgs();
 		}
 
-		this.isRunning = false;
-
 		return outputs;
 	}
 
-	// TODO: Merge with run method by providing a predicate
+	public LinkedList<Integer> run(LinkedList<Integer> inputs) {
+		return run(inputs, numOutputs -> {
+			return true;
+		});
+	}
+
 	public LinkedList<Integer> runToNextOutput() {
-		while (outputs.size() == 0 && isRunning && instructionPointer < memory.length) {
-			int currentInstruction = memory[instructionPointer];
-			OpParams op = OpParams.getByInstruction(currentInstruction);
-			int args[] = new int[4];
+		return run(inputs, numOutputs -> {
+			return numOutputs == 0;
+		});
 
-			for (int i = 0; i < op.operation.getNumArgs() - 1; i++) {
-				args[i] = memory[instructionPointer + (i + 1)];
-			}
-
-			switch (op.operation) {
-			case SUM:
-				memory[args[2]] = getArgValue(op.arg1Mode, memory, args[0]) + getArgValue(op.arg2Mode, memory, args[1]);
-				break;
-			case MULTIPLY:
-				memory[args[2]] = getArgValue(op.arg1Mode, memory, args[0]) * getArgValue(op.arg2Mode, memory, args[1]);
-				break;
-			case STORE:
-				memory[args[0]] = inputs.removeFirst();
-				break;
-			case OUTPUT:
-				outputs.push(getArgValue(op.arg1Mode, memory, args[0]));
-				break;
-			case EXIT:
-				this.isRunning = false;
-				break;
-			case JUMP_IF_TRUE:
-				instructionPointer = getArgValue(op.arg1Mode, memory, args[0]) != 0
-						? getArgValue(op.arg2Mode, memory, args[1])
-						: instructionPointer + op.operation.getNumArgs();
-				continue;
-			case JUMP_IF_FALSE:
-				instructionPointer = getArgValue(op.arg1Mode, memory, args[0]) == 0
-						? getArgValue(op.arg2Mode, memory, args[1])
-						: instructionPointer + op.operation.getNumArgs();
-				continue;
-			case LESS_THAN:
-				memory[args[2]] = getArgValue(op.arg1Mode, memory, args[0]) < getArgValue(op.arg2Mode, memory, args[1])
-						? 1
-						: 0;
-				break;
-			case EQUALS:
-				memory[args[2]] = getArgValue(op.arg1Mode, memory, args[0]) == getArgValue(op.arg2Mode, memory, args[1])
-						? 1
-						: 0;
-				break;
-			default:
-				break;
-			}
-			instructionPointer += op.operation.getNumArgs();
-		}
-
-		return outputs;
 	}
 
 	public void addInput(int input) {
